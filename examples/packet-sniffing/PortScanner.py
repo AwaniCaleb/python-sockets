97d92737533636efe43cbc7f6d911264fd2ca36e
Awani Caleb <calebawani@gmail.com>
2025-08-22 23:30:14 +0100

    feat: create a basic port scanner,  IP layer and TCP SYN packet for the port scanner

---
+++ examples/packet-sniffing/PortScanner.py
@@ -0,0 +1,17 @@
+import socket
+from scapy.all import IP, TCP
+
+class PortScanner():
+    DEFAULT_TARGET = socket.gethostbyname(socket.gethostname())
+    DEFAULT_PORT = 80
+
+    def __init__(self, target: str, port: int):
+        self.socket = None
+        self.target = target if target else self.DEFAULT_TARGET
+        self.port = port if port else self.DEFAULT_PORT
+
+    def scan_port(self):
+        target_ip = IP(dst=self.target)
+        tcp_packet = TCP(dport=self.port, flags="S")
+
+        syn_packet = target_ip / tcp_packet
